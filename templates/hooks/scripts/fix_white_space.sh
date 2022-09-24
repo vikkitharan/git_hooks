b8d3b4747cca10d0a87013f299a84f5e408aa8c2
Vikki <vikki.gsam@gmail.com>
2022-09-24 16:30:47 +1000

    Update version

--- templates/hooks/scripts/fix_white_space.sh
+++ templates/hooks/scripts/fix_white_space.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 #         File: fix_white_space.sh
-#               Fix new trailing whitespaces and blank lines at end of file.
+#               Remove new trailing whitespaces and blank lines at end of file.
 #   Created by: vikki
 #   Created on: 2022/09/21
 #  Modified by: vikki
-#  Modified on: 2022/09/24
+#  Modified on: 2022/09/25
 #      Version: 1.0.0
 #*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-
-#test cases
-# 1. Adding file s
-#   a. no trailing whitespace or blank lines: no action
-#   b. with a trailing whitespace: remove the traling whitespace
-#   c. with a blank line at EOF: remove the balnk line EOF
-#   d. with multiple traling whitespaces and multiple blank lines: remove the traling whitespalces and blank lines
-#   e. with multiple traling whitespaces and multiple blank lines on multiple files: remove the traling whitespalces and blank lines
-# 2. Editing an existing file
-#   A. no new trailing whitespace or blank lines: no action
-#   B. with a new trailing whitespace: remove the new traling whitespace
-#   C. with a new blank line at EOF: remove the new balnk line EOF
-#   D. with new multiple traling whitespaces and multiple blank lines: remove the traling new whitespalces and blank lines
-#   E. with new multiple traling whitespaces and multiple blank lines on multiple files: remove new the traling whitespalces and blank lines
-
 
 # Set the  internal field separator to split lines by new line character.
 IFS=$'\n'
@@ -35,10 +20,8 @@ else
 	against=$(git hash-object -t tree /dev/null)
 fi
 
-
 # array of badly formated files
 bad_files=()
-
 
 for line in `git diff-index --check --cached $against -- | sed '/^[+-]/d'` ; do
   file=($(echo $line | sed -r 's/:[0-9]+: .*//'))
