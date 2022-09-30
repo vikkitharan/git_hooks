5cd75f8183fe9aaa4cf88374376fab9788d67215
Vikkitharan Gnanasambandapillai <vikki.gsam@seeingmachines.com>
2022-09-30 12:55:45 +1000

    Update fix_multiple_whitespaces.sh

--- fix_multiple_whitespaces.sh
+++ fix_multiple_whitespaces.sh
@@ -1,22 +1,62 @@
 #!/bin/bash
 #-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
-#         File: fix_multiple_whitespaces.sh
-#               Remove multiple whitespaces within the text
+#     	File: fix_multiple_whitespaces.sh
+#           	Remove multiple whitespaces within the text
 #   Created by: vikki
 #   Created on: 2022/09/27
 #  Modified by: vikki
 #  Modified on: 2022/09/27
-#      Version: 0.0.0
+#  	Version: 1.0.0
 #*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-# TODO 1. Identify line numbers which are
-#         (a) added or modified in staged and
-#         (b) have multiple non leading whitespaces
-#      loop over git diff --cached -U output
-#         if line is added or modified
-#           if the line has multiple non leading whitespaces
-#             call map()
-# TODO 2. Map the line numbers from staged to local files.
-#         (a) Generate mapper data structure using git diff -U
-#         (b) map using line input and the data structure
-# TODO 3. Replace non leading multiple white space by a single space
+
+#########################################
+# find line numbers which are added or modified in staged
+#########################################
+
+find_cached_files_lines () {
+  git diff --cached -U | gawk '
+  match($0,"^+++ b/([[:alnum:]/._]+)", files){
+  file=files[1]
+}
+
+match($0,"^@@ -([0-9]+),([0-9]+) [+]([0-9]+),([0-9]+) @@", chunk_header){
+line=chunk_header[3]
+}
+
+{ line_conent=substr($0,2) }
+
+/^(---|[^-+ ])/{next }
+/^[-]/{ next }
+/^[+][^+]/{ printf "%s,%s ",file, line++;next;}
+{ line++ }
+'
+}
+
+
+###########################################
+# map the line number from cached to local
+#     	(a) Generate mapper data structure using git diff -U
+#     	(b) map using line input and the data structure
+###########################################
+map_cached_to_local() {
+  printf "%s" "$1"
+}
+
+
+###########################################
+# Replace non leading multiple white space by a single space
+###########################################
+fix_white_space() {
+  for file_line in $(echo "$files_lines" | tr " " "\n");
+  do
+    file=$(echo "$file_line" | cut -d "," -f1)
+    line_number_cached=$(echo "$file_line" | cut -d "," -f2)
+    line_number_local=$(map_cached_to_local "$line_number_cached")
+    sed -i "${line_number_local}s/ \{1,\}/ /g" "$file"
+  done
+}
+
+files_lines=$(find_cached_files_lines)
+
+fix_white_space
