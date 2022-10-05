af383a13414432fa19a7f4fd0c0ed84724627afa
Vikki <vikki.gsam@gmail.com>
2022-10-06 10:05:48 +1100

    Remove leading space(s) followed by tab(s)

--- fix_multiple_whitespaces.sh
+++ fix_multiple_whitespaces.sh
@@ -54,8 +54,8 @@ map_cached_to_local() {
 # Replace non leading multiple white space by a single space
 #  (1) No action for trailing whitespaces
 #  (2) Do not remove leading multiple spaces, tabs or tabs followed by spaces
-#  (3) + Remove multiple middle whitespaces
-#  (4) Remove leading spaces followed by a tab
+#  (3) Substitute two or more spaces or tab between words by single space or tab
+#  (4) Remove leading space(s) followed by tab(s)
 ###########################################
 fix_white_space() {
   for file_line in $(echo "$files_lines" | tr " " "\n");
@@ -67,6 +67,8 @@ fix_white_space() {
     # substitute two or more spaces or tab between words by single space or tab
     sed -i "${line_number_local}s/\>\([[:blank:]]\)\{2,\}/\1/g" "$file"
 
+    # remove leading space(s) followed by tab(s)
+    sed -i "${line_number_local}s/^ \{1,\}\(\t\{1,\}\)/\1/g" "$file"
   done
 }
 
